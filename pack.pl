f90c33effb2191cbcb7efd62e96b5345eeeb7cdb
root <logicmoo@gmail.com>
2017-02-23 07:23:32 -0800

    all submodules

--- pack.pl
+++ pack.pl
@@ -5,6 +5,7 @@ keywords([reader,dcg,sexpr,pddl]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
-home( 'https://github.com/TeamSPoon/s_expression' ).
-download( 'https://github.com/TeamSPoon/s_expression/release/*.zip' ).
+home( 'https://github.com/TeamSPoon/pack_s_expression' ).
+download( 'https://github.com/TeamSPoon/pack_s_expression/release/*.zip' ).
 requires(with_open_options).
+autoload(true).
